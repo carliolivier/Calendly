bcfe00c28aabb6c8264f5eb13d4aca675742441b
GUILLOT Jonathan <90567262+John-athan14@users.noreply.github.com>
2023-02-21 19:10:13 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  env: {
-    MAILSLURP_API_KEY:
-      '0eb8c10df01cb2342a0ebe4f6e25ad1a56d490da7a22751db6c21932bea2c897P',
-  },
+ 
   e2e: {
     defaultCommandTimeout: 40000,
     responseTimeout: 40000,
