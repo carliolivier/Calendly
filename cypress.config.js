92c12f2adc448b19fbd32b8607c0997295d0d59d
John-athan14 <guillot.jonathan59@gmail.com>
2023-02-21 16:42:19 +0100

    Add Création of Event

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  env: {
+    MAILSLURP_API_KEY:
+      '0eb8c10df01cb2342a0ebe4f6e25ad1a56d490da7a22751db6c21932bea2c897P',
+  },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    defaultCommandTimeout: 40000,
+    responseTimeout: 40000,
+    requestTimeout: 40000,
   },
 });
---
+++ cypress/e2e/Projet3.cy.js
@@ -0,0 +1,23 @@
+describe('', () => {
+  it("Création d'un évenement", () => {
+    cy.visit(
+      'https://calendly.com/users/confirmation?confirmation_token=VsSGnfaEmapBZpwKsSNm'
+    );
+    cy.get('[name="password"]').click().type('jonathanTestCalendly123');
+    cy.contains('Continuer').click();
+    cy.get('#onetrust-reject-all-handler').click();
+    cy.get(
+      '.js-new-event-type-region > :nth-child(1) > .M5lLwMMp9XR_Ejbv83RK > .clyOGp1YInfwtQ8ffY_1 > .M_4ZJy_elGKAc65Rl6DH > ._6n0CRU_Q9ef6f3sgBGZ'
+    ).click();
+    cy.get(
+      '[data-component="group"] > .qAwyEnpbreTCu2tzPMOi > ._QVEO42ALEjm3yr8WTJV'
+    ).click();
+    cy.get('[name="name"]').click().type('Cypress');
+    cy.get('.PAft3yNcTUafvfdpD_me').click();
+    cy.contains('Google Meet').click();
+    cy.get('[name="inviteesLimit"]').type('4');
+    cy.contains('Suivant').click();
+    cy.contains('Suivant').click();
+    cy.contains('Retour').click();
+  });
+});
