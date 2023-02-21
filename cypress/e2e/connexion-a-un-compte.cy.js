af7b082abf15845fb406ff3707f602e77d66574e
carliolivier <olivier.carlier.foreach@gmail.com>
2023-02-21 16:09:11 +0100

    one

--- cypress/e2e/connexion-a-un-compte.cy.js
+++ cypress/e2e/connexion-a-un-compte.cy.js
@@ -1,13 +1,23 @@
 /// <reference types="cypress" />
 
 describe("connexion reussi a un compre", () => {
-	it("insciption", () => {
-        cy.visit("https://calendly.com/");
-        cy.get('#onetrust-accept-btn-handler').click();
-        cy.wait(3000);
-        cy.get('#right-side-components > :nth-child(1) > .sc-1e4rkx0-0').click();
-        cy.wait(3000);
-        // cy.get('.sc-r1giur-0').type('zitoune59200@yahoo.com');
+	it("connexion", () => {
+                   
+                        cy.visit("https://calendly.com/");
+                        cy.wait(3000);
+                        cy.get('#onetrust-accept-btn-handler').click();
+                        cy.wait(3000);
+                        cy.get('#right-side-components > :nth-child(1) > .sc-1e4rkx0-0').click();
+                        cy.wait(3000);
+                        cy.get('.sc-r1giur-0').type('zitoune59200@yahoo.com', {force:true});
+                        cy.get('[data-testid="primary-button"]').click({force:true});
+                        cy.get('input[name="password"]').type('Jenifer84*');
+                        cy.get("#talon").click({force:true});
+                        cy.wait(2000);
+                       
+                      
+                      
+                        });
+                });
+   
   
-	});
-});
