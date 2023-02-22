970b620e20db9d859a7e09046a2a34e00f4e7c0c
John-athan14 <guillot.jonathan59@gmail.com>
2023-02-22 09:36:14 +0100

    Add delete of an event

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,3 @@
 const { defineConfig } = require('cypress');
 
-module.exports = defineConfig({
-  env: {
-    MAILSLURP_API_KEY:
-      '0eb8c10df01cb2342a0ebe4f6e25ad1a56d490da7a22751db6c21932bea2c897P',
-  },
-  e2e: {
-    defaultCommandTimeout: 40000,
-    responseTimeout: 40000,
-    requestTimeout: 40000,
-  },
-});
+module.exports = defineConfig({});
